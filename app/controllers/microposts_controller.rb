8642ab1a1b7b2aee903577ba37bd94a0db3ab5e6
Akiko Takano <akiko.pusu@gmail.com>
2018-10-02 15:35:09 +0900

    Code refactor.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,6 @@
 class MicropostsController < ApplicationController
   def create
     # RuboCop will say "binding.pry is a debug code, so you should remove it.".
-    binding.pry
     content = params.require(:micropost).require(:content)
     # RuboCop will say "Use `create!` instead of `create` because `create` ignores validation errors.".
     current_user.microposts.create(content: content)
